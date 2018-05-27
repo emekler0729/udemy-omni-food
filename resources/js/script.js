12713968ff674dd5c99e20f65b5d5de1dc4dd342
Eduard Mekler <emekler0729@gmail.com>
2018-05-27 12:57:12 -0500

    Added scroll animation to nav and landing page buttons

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-    $('.js--section-features').waypoint(function (direction) {
+$(document).ready(function() {
+
+    /* Sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
         if(direction == 'down') {
             $('nav').addClass('sticky');
         } else {
@@ -7,5 +9,34 @@ $(document).ready(function () {
         }
     }, {
         offset: '60px;'
+    });
+
+    /* Scroll on button press */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000)
+    })
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000)
+    })
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if(target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
     });
 });
